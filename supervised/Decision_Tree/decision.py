dc89dfc5e2989167880ea7fec2de70ecb79802b3
Omid <omdiekz@gmail.com>
2020-02-13 13:42:53 +0330

    make generic model and methods

--- supervised/Decision_Tree/decision.py
+++ supervised/Decision_Tree/decision.py
@@ -18,44 +18,44 @@ class Model(ABC):
     def get_value(self, key):
         ...
 
-    @staticmethod
-    def ig(ant, instances, attr, domain: list) -> float:
-        if ant is None:
-            ant = Model.entropy(instances)
-        instances: List[Model] = instances
-        branches = Model.make_branches(attr, domain, instances)
-        for pos, neg, pl, nl in branches.values():
-            ant -= (pos + neg) / len(instances) * Model.entropy_zero(pos, neg, pos + neg)
-        return ant
 
-    @staticmethod
-    def make_branches(attr, domain, instances):
-        branches = dict([(d, [0, 0, [], []]) for d in domain])
-        for ins in instances:
-            key = ins.get_value(attr)
-            if ins.is_positive:
-                branches.get(key)[0] += 1
-                branches.get(key)[2].append(ins)
-            else:
-                branches.get(key)[1] += 1
-                branches.get(key)[3].append(ins)
-        return branches
+def make_branches(attr, domain, instances):
+    branches = dict([(d, [0, 0, [], []]) for d in domain])
+    for ins in instances:
+        key = ins.get_value(attr)
+        if ins.is_positive:
+            branches.get(key)[0] += 1
+            branches.get(key)[2].append(ins)
+        else:
+            branches.get(key)[1] += 1
+            branches.get(key)[3].append(ins)
+    return branches
 
-    @staticmethod
-    def entropy(instances):
-        pos = 0
-        neg = 0
-        for ins in instances:
-            if ins.is_positive:
-                pos += 1
-            else:
-                neg += 1
-        return Model.entropy_zero(pos, neg, len(instances))
 
-    @staticmethod
-    def entropy_zero(pos, neg, _all):
-        if _all == 0:
-            return 0
-        pp = pos / _all
-        pn = neg / _all
-        return -pp * (math.log2(pp) if pp != 0 else 0) - pn * (math.log2(pn) if pn != 0 else 0)
+def entropy(instances):
+    pos = 0
+    neg = 0
+    for ins in instances:
+        if ins.is_positive:
+            pos += 1
+        else:
+            neg += 1
+    return entropy_zero(pos, neg, len(instances))
+
+
+def entropy_zero(pos, neg, _all):
+    if _all == 0:
+        return 0
+    pp = pos / _all
+    pn = neg / _all
+    return -pp * (math.log2(pp) if pp != 0 else 0) - pn * (math.log2(pn) if pn != 0 else 0)
+
+
+def ig(ant, instances, attr, domain: list) -> float:
+    if ant is None:
+        ant = entropy(instances)
+    instances: List[Model] = instances
+    branches = make_branches(attr, domain, instances)
+    for pos, neg, pl, nl in branches.values():
+        ant -= (pos + neg) / len(instances) * entropy_zero(pos, neg, pos + neg)
+    return ant
